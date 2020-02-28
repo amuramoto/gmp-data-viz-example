c1450c8fa946aa7dfc089438244220fbcf5cda60
Alex Muramoto <amuramoto@apache.org>
2020-02-27 16:00:58 -0800

    some data stuff

--- src/app.js
+++ src/app.js
@@ -3,7 +3,22 @@ import {GoogleMapLoader} from './map_loader.js';
 // Set your Google Maps API key here or via environment variable
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey || "";
 (async function() {
-  const MAP = await GoogleMapLoader.init(GOOGLE_MAPS_API_KEY);
+  const map = await GoogleMapLoader.init(GOOGLE_MAPS_API_KEY);
+  map.data.loadGeoJson('../data/counties.json');
 }())
 
+function addMarkers(){
 
+}
+
+function addMarkClusters(){
+
+}
+
+function addPolylines(){
+
+}
+
+function addPolygons(){
+
+}
